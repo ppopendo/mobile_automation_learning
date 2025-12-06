14f0be6ecd87701b9bd14b05a636a4d3460f06c8
PP <69356532+ppopendo@users.noreply.github.com>
2025-12-06 14:40:28 +0100

    [mobile]refactor: added modulariztion and  pre-commit hooks pylint/black

--- pages/checkout_payment_page.py
+++ pages/checkout_payment_page.py
@@ -7,128 +7,117 @@ from .base_page import BasePage
 
 @dataclass(frozen=True)
 class CheckoutPaymentPageLocators:
-	PAGE_HEADER: Tuple[str, str] = field(
-		default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/enterPaymentTitleTV"),
-		init=False
-	)
-	PAGE_SUB_TITLE: Tuple[str, str] = field(
-		default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/enterPaymentMethodTV"),
-		init=False
-	)
-	CARD_HEADER: Tuple[str, str] = field(
-		default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/cardTV"),
-		init=False
-	)
-	FULL_NAME_INPUT: Tuple[str, str] = field(
-		default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/nameET"),
-		init=False
-	)
-	CARD_NUMBER_INPUT: Tuple[str, str] = field(
-		default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/cardNumberET"),
-		init=False
-	)
-	EXPIRATION_DATE_INPUT: Tuple[str, str] = field(
-		default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/expirationDateET"),
-		init=False
-	)
-	SECURITY_CODE_INPUT: Tuple[str, str] = field(
-		default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/securityCodeET"),
-		init=False
-	)
-	BILLING_ADDRESS_SAME_AS_SHIPPING_CHECKBOX: Tuple[str, str] = field(
-		default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/billingAddressCB"),
-		init=False
-	)
-	REVIEW_ORDER_BUTTON: Tuple[str, str] = field(
-		default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/paymentBtn"),
-		init=False
-	)
-	FULL_NAME_ERROR_MESSAGE: Tuple[str, str] = field(
-		default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/nameErrorTV"),
-		init=False
-	)
-	FULL_NAME_ERROR_ICON: Tuple[str, str] = field(
-		default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/nameErrorIV"),
-		init=False
-	)
-	CARD_NUMBER_ERROR_ICON: Tuple[str, str] = field(
-		default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/cardNumberErrorIV"),
-		init=False
-	)
-	EXPIRATION_DATE_ERROR_MESSAGE: Tuple[str, str] = field(
-		default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/expirationDateErrorTV"),
-		init=False
-	)
-	EXPIRATION_DATE_ERROR_ICON: Tuple[str, str] = field(
-		default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/expirationDateIV"),
-		init=False
-	)
-	SECURITY_CODE_ERROR_MESSAGE: Tuple[str, str] = field(
-		default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/securityCodeErrorTV"),
-		init=False
-	)
-	SECURITY_CODE_ERROR_ICON: Tuple[str, str] = field(
-		default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/securityCodeIV"),
-		init=False
-	)
+    PAGE_HEADER: Tuple[str, str] = field(
+        default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/enterPaymentTitleTV"), init=False
+    )
+    PAGE_SUB_TITLE: Tuple[str, str] = field(
+        default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/enterPaymentMethodTV"), init=False
+    )
+    CARD_HEADER: Tuple[str, str] = field(default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/cardTV"), init=False)
+    FULL_NAME_INPUT: Tuple[str, str] = field(
+        default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/nameET"), init=False
+    )
+    CARD_NUMBER_INPUT: Tuple[str, str] = field(
+        default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/cardNumberET"), init=False
+    )
+    EXPIRATION_DATE_INPUT: Tuple[str, str] = field(
+        default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/expirationDateET"), init=False
+    )
+    SECURITY_CODE_INPUT: Tuple[str, str] = field(
+        default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/securityCodeET"), init=False
+    )
+    BILLING_ADDRESS_SAME_AS_SHIPPING_CHECKBOX: Tuple[str, str] = field(
+        default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/billingAddressCB"), init=False
+    )
+    REVIEW_ORDER_BUTTON: Tuple[str, str] = field(
+        default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/paymentBtn"), init=False
+    )
+    FULL_NAME_ERROR_MESSAGE: Tuple[str, str] = field(
+        default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/nameErrorTV"), init=False
+    )
+    FULL_NAME_ERROR_ICON: Tuple[str, str] = field(
+        default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/nameErrorIV"), init=False
+    )
+    CARD_NUMBER_ERROR_ICON: Tuple[str, str] = field(
+        default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/cardNumberErrorIV"), init=False
+    )
+    EXPIRATION_DATE_ERROR_MESSAGE: Tuple[str, str] = field(
+        default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/expirationDateErrorTV"), init=False
+    )
+    EXPIRATION_DATE_ERROR_ICON: Tuple[str, str] = field(
+        default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/expirationDateIV"), init=False
+    )
+    SECURITY_CODE_ERROR_MESSAGE: Tuple[str, str] = field(
+        default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/securityCodeErrorTV"), init=False
+    )
+    SECURITY_CODE_ERROR_ICON: Tuple[str, str] = field(
+        default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/securityCodeIV"), init=False
+    )
 
 
 class CheckoutPaymentPage(BasePage):
 
-	@allure.step("the user waits until the checkout payment page is displayed")
-	def wait_until_page_is_loaded(self, timeout=10) -> None:
-		expected_locators = [
-			CheckoutPaymentPageLocators.PAGE_HEADER,
-			CheckoutPaymentPageLocators.PAGE_SUB_TITLE,
-			CheckoutPaymentPageLocators.CARD_HEADER,
-			CheckoutPaymentPageLocators.FULL_NAME_INPUT,
-			CheckoutPaymentPageLocators.CARD_NUMBER_INPUT,
-			CheckoutPaymentPageLocators.EXPIRATION_DATE_INPUT,
-			CheckoutPaymentPageLocators.SECURITY_CODE_INPUT,
-			CheckoutPaymentPageLocators.BILLING_ADDRESS_SAME_AS_SHIPPING_CHECKBOX,
-			CheckoutPaymentPageLocators.REVIEW_ORDER_BUTTON,
-		]
-		for locator in expected_locators:
-			self.wait_for_element(locator, timeout=timeout)
+    @allure.step("the user waits until the checkout payment page is displayed")
+    def wait_until_page_is_loaded(self, timeout=10) -> None:
+        expected_locators = [
+            CheckoutPaymentPageLocators.PAGE_HEADER,
+            CheckoutPaymentPageLocators.PAGE_SUB_TITLE,
+            CheckoutPaymentPageLocators.CARD_HEADER,
+            CheckoutPaymentPageLocators.FULL_NAME_INPUT,
+            CheckoutPaymentPageLocators.CARD_NUMBER_INPUT,
+            CheckoutPaymentPageLocators.EXPIRATION_DATE_INPUT,
+            CheckoutPaymentPageLocators.SECURITY_CODE_INPUT,
+            CheckoutPaymentPageLocators.BILLING_ADDRESS_SAME_AS_SHIPPING_CHECKBOX,
+            CheckoutPaymentPageLocators.REVIEW_ORDER_BUTTON,
+        ]
+        for locator in expected_locators:
+            self.wait_for_element(locator, timeout=timeout)
 
-	def enter_full_name(self, full_name: str) -> None:
-		with allure.step(f"the user enters full name: {full_name}"):
-			full_name_input = self.wait_for_element(CheckoutPaymentPageLocators.FULL_NAME_INPUT)
-			full_name_input.clear()
-			full_name_input.send_keys(full_name)
+    def enter_full_name(self, full_name: str) -> None:
+        with allure.step(f"the user enters full name: {full_name}"):
+            full_name_input = self.wait_for_element(CheckoutPaymentPageLocators.FULL_NAME_INPUT)
+            full_name_input.clear()
+            full_name_input.send_keys(full_name)
 
-	def enter_card_number(self, card_number: str) -> None:
-		with allure.step(f"the user enters card number: {card_number}"):
-			card_number_input = self.wait_for_element(CheckoutPaymentPageLocators.CARD_NUMBER_INPUT)
-			card_number_input.clear()
-			card_number_input.send_keys(card_number)
+    def enter_card_number(self, card_number: str) -> None:
+        with allure.step(f"the user enters card number: {card_number}"):
+            card_number_input = self.wait_for_element(CheckoutPaymentPageLocators.CARD_NUMBER_INPUT)
+            card_number_input.clear()
+            card_number_input.send_keys(card_number)
 
-	def enter_expiration_date(self, expiration_date: str) -> None:
-		with allure.step(f"the user enters expiration date: {expiration_date}"):
-			expiration_date_input = self.wait_for_element(CheckoutPaymentPageLocators.EXPIRATION_DATE_INPUT)
-			expiration_date_input.clear()
-			expiration_date_input.send_keys(expiration_date)
+    def enter_expiration_date(self, expiration_date: str) -> None:
+        with allure.step(f"the user enters expiration date: {expiration_date}"):
+            expiration_date_input = self.wait_for_element(CheckoutPaymentPageLocators.EXPIRATION_DATE_INPUT)
+            expiration_date_input.clear()
+            expiration_date_input.send_keys(expiration_date)
 
-	def enter_security_code(self, security_code: str) -> None:
-		with allure.step(f"the user enters security code: {security_code}"):
-			security_code_input = self.wait_for_element(CheckoutPaymentPageLocators.SECURITY_CODE_INPUT)
-			security_code_input.clear()
-			security_code_input.send_keys(security_code)
+    def enter_security_code(self, security_code: str) -> None:
+        with allure.step(f"the user enters security code: {security_code}"):
+            security_code_input = self.wait_for_element(CheckoutPaymentPageLocators.SECURITY_CODE_INPUT)
+            security_code_input.clear()
+            security_code_input.send_keys(security_code)
 
-	def toggle_billing_address_checkbox(self) -> None:
-		with allure.step("the user toggles the billing address same as shipping checkbox"):
-			checkbox = self.wait_for_element(CheckoutPaymentPageLocators.BILLING_ADDRESS_SAME_AS_SHIPPING_CHECKBOX)
-			checkbox.click()
+    def toggle_billing_address_checkbox(self) -> None:
+        with allure.step("the user toggles the billing address same as shipping checkbox"):
+            checkbox = self.wait_for_element(CheckoutPaymentPageLocators.BILLING_ADDRESS_SAME_AS_SHIPPING_CHECKBOX)
+            checkbox.click()
 
-	@allure.step("the user clicks the 'Review Order' button")
-	def click_review_order_button(self) -> None:
-		self.tap_element(CheckoutPaymentPageLocators.REVIEW_ORDER_BUTTON)
+    @allure.step("the user clicks the 'Review Order' button")
+    def click_review_order_button(self) -> None:
+        self.tap_element(CheckoutPaymentPageLocators.REVIEW_ORDER_BUTTON)
 
-	@allure.step("the user enters the payment information")
-	def enter_payment_information(self, full_name: str, card_number: str, expiration_date: str, security_code: str, billing_address_same_as_shipping: bool = False) -> None:
-		self.enter_full_name(full_name)
-		self.enter_card_number(card_number)
-		self.enter_expiration_date(expiration_date)
-		self.enter_security_code(security_code)
-		if billing_address_same_as_shipping:
-			self.toggle_billing_address_checkbox()
+    @allure.step("the user enters the payment information")
+    def enter_payment_information(
+        self,
+        full_name: str,
+        card_number: str,
+        expiration_date: str,
+        security_code: str,
+        billing_address_same_as_shipping: bool = False,
+    ) -> None:
+        self.enter_full_name(full_name)
+        self.enter_card_number(card_number)
+        self.enter_expiration_date(expiration_date)
+        self.enter_security_code(security_code)
+        if billing_address_same_as_shipping:
+            self.toggle_billing_address_checkbox()
--- pages/product_details_page.py
+++ pages/product_details_page.py
@@ -7,125 +7,115 @@ from .base_page import BasePage
 
 @dataclass(frozen=True)
 class ProductDetailsPageLocators:
-	PRODUCT_COLOR: Tuple[str, str] = field(
-		default=(AppiumBy.XPATH, "//android.widget.ImageView[@content-desc='{product_color}']"),
-		init=False
-	)
-	PRODUCT_PRICE: Tuple[str, str] = field(
-		default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/priceTV"),
-		init=False
-	)
-	PRODUCT_DESCRIPTION: Tuple[str, str] = field(
-		default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/descTV"),
-		init=False
-	)
-	PRODUCT_HIGHLIGHTS: Tuple[str, str] = field(
-		default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/productHeightLightsTV"),
-		init=False
-	)
-	INCREASE_QUANTITY_BUTTON: Tuple[str, str] = field(
-		default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/plusIV"),
-		init=False
-	)
-	DECREASE_QUANTITY_BUTTON: Tuple[str, str] = field(
-		default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/minusIV"),
-		init=False
-	)
-	COUNT_ITEM: Tuple[str, str] = field(
-		default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/noTV"),
-		init=False
-	)
-	ADD_TO_CART_BUTTON: Tuple[str, str] = field(
-		default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/cartBt"),
-		init=False
-	)
-	CART_ICON: Tuple[str, str] = field(
-		default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/cartIV"),
-		init=False
-	)
-	COUNT_ITEM_IN_CART: Tuple[str, str] = field(
-		default=(AppiumBy.XPATH, "//android.widget.RelativeLayout[@content-desc='Displays number of items in your cart']/android.widget.ImageView"),
-		init=False
-	)
-	PRODUCT_HEADER: Tuple[str, str] = field(
-		default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/productTV"),
-		init=False
-	)
-	COUNT_ITEM_IN_CART_VALUE: Tuple[str, str] = field(
-		default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/cartTV"),
-		init=False
-	)
+    PRODUCT_COLOR: Tuple[str, str] = field(
+        default=(AppiumBy.XPATH, "//android.widget.ImageView[@content-desc='{product_color}']"), init=False
+    )
+    PRODUCT_PRICE: Tuple[str, str] = field(
+        default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/priceTV"), init=False
+    )
+    PRODUCT_DESCRIPTION: Tuple[str, str] = field(
+        default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/descTV"), init=False
+    )
+    PRODUCT_HIGHLIGHTS: Tuple[str, str] = field(
+        default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/productHeightLightsTV"), init=False
+    )
+    INCREASE_QUANTITY_BUTTON: Tuple[str, str] = field(
+        default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/plusIV"), init=False
+    )
+    DECREASE_QUANTITY_BUTTON: Tuple[str, str] = field(
+        default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/minusIV"), init=False
+    )
+    COUNT_ITEM: Tuple[str, str] = field(default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/noTV"), init=False)
+    ADD_TO_CART_BUTTON: Tuple[str, str] = field(
+        default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/cartBt"), init=False
+    )
+    CART_ICON: Tuple[str, str] = field(default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/cartIV"), init=False)
+    COUNT_ITEM_IN_CART: Tuple[str, str] = field(
+        default=(
+            AppiumBy.XPATH,
+            "//android.widget.RelativeLayout[@content-desc='Displays number of items in your cart']"
+            "/android.widget.ImageView",
+        ),
+        init=False,
+    )
+    PRODUCT_HEADER: Tuple[str, str] = field(
+        default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/productTV"), init=False
+    )
+    COUNT_ITEM_IN_CART_VALUE: Tuple[str, str] = field(
+        default=(AppiumBy.ID, "com.saucelabs.mydemoapp.android:id/cartTV"), init=False
+    )
 
 
 class ProductDetailsPage(BasePage):
 
-	@allure.step("the user waits until the product details page is displayed")
-	def wait_until_page_is_loaded(self, timeout=10) -> None:
-		expected_locators = [
-			ProductDetailsPageLocators.PRODUCT_HEADER,
-			ProductDetailsPageLocators.PRODUCT_PRICE,
-			ProductDetailsPageLocators.PRODUCT_HIGHLIGHTS,
-			ProductDetailsPageLocators.PRODUCT_DESCRIPTION,
-			ProductDetailsPageLocators.INCREASE_QUANTITY_BUTTON,
-			ProductDetailsPageLocators.DECREASE_QUANTITY_BUTTON,
-			ProductDetailsPageLocators.COUNT_ITEM,
-			ProductDetailsPageLocators.ADD_TO_CART_BUTTON,
-			ProductDetailsPageLocators.CART_ICON,
-		]
-		for locator in expected_locators:
-			self.wait_for_element(locator, timeout=timeout, scroll_to_element=True)
+    @allure.step("the user waits until the product details page is displayed")
+    def wait_until_page_is_loaded(self, timeout=10) -> None:
+        expected_locators = [
+            ProductDetailsPageLocators.PRODUCT_HEADER,
+            ProductDetailsPageLocators.PRODUCT_PRICE,
+            ProductDetailsPageLocators.PRODUCT_HIGHLIGHTS,
+            ProductDetailsPageLocators.PRODUCT_DESCRIPTION,
+            ProductDetailsPageLocators.INCREASE_QUANTITY_BUTTON,
+            ProductDetailsPageLocators.DECREASE_QUANTITY_BUTTON,
+            ProductDetailsPageLocators.COUNT_ITEM,
+            ProductDetailsPageLocators.ADD_TO_CART_BUTTON,
+            ProductDetailsPageLocators.CART_ICON,
+        ]
+        for locator in expected_locators:
+            self.wait_for_element(locator, timeout=timeout, scroll_to_element=True)
 
-	allure.step("the user adds the product to the cart")
-	def click_add_to_cart_button(self) -> None:
-		self.tap_element(ProductDetailsPageLocators.ADD_TO_CART_BUTTON)
+    allure.step("the user adds the product to the cart")
 
-	@property
-	@allure.step("retrieving product price")
-	def product_price(self) -> str:
-		return self.wait_for_element(ProductDetailsPageLocators.PRODUCT_PRICE).text
+    def click_add_to_cart_button(self) -> None:
+        self.tap_element(ProductDetailsPageLocators.ADD_TO_CART_BUTTON)
 
-	@property
-	@allure.step("retrieving product description")
-	def product_description(self) -> str:
-		return self.wait_for_element(ProductDetailsPageLocators.PRODUCT_DESCRIPTION).text
+    @property
+    @allure.step("retrieving product price")
+    def product_price(self) -> str:
+        return self.wait_for_element(ProductDetailsPageLocators.PRODUCT_PRICE).text
 
-	@property
-	@allure.step("retrieving product highlights")
-	def product_highlights(self) -> str:
-		return self.wait_for_element(ProductDetailsPageLocators.PRODUCT_HIGHLIGHTS).text
+    @property
+    @allure.step("retrieving product description")
+    def product_description(self) -> str:
+        return self.wait_for_element(ProductDetailsPageLocators.PRODUCT_DESCRIPTION).text
 
-	@property
-	@allure.step("retrieving count of items")
-	def item_count(self) -> int:
-		return int(self.wait_for_element(ProductDetailsPageLocators.COUNT_ITEM).text)
+    @property
+    @allure.step("retrieving product highlights")
+    def product_highlights(self) -> str:
+        return self.wait_for_element(ProductDetailsPageLocators.PRODUCT_HIGHLIGHTS).text
 
-	@property
-	@allure.step("retrieving product color element")
-	def product_color(self):
-		return self.wait_for_element(ProductDetailsPageLocators.PRODUCT_COLOR)
+    @property
+    @allure.step("retrieving count of items")
+    def item_count(self) -> int:
+        return int(self.wait_for_element(ProductDetailsPageLocators.COUNT_ITEM).text)
 
-	@allure.step("the user opens the cart page by clicking the cart icon")
-	def click_cart_icon(self) -> None:
-		self.tap_element(ProductDetailsPageLocators.CART_ICON)
+    @property
+    @allure.step("retrieving product color element")
+    def product_color(self):
+        return self.wait_for_element(ProductDetailsPageLocators.PRODUCT_COLOR)
 
-	@allure.step("the user increases the item count by clicking the '+' button")
-	def increase_item_count(self, items_count: int) -> None:
-		for _ in range(items_count):
-			self.tap_element(ProductDetailsPageLocators.INCREASE_QUANTITY_BUTTON)
+    @allure.step("the user opens the cart page by clicking the cart icon")
+    def click_cart_icon(self) -> None:
+        self.tap_element(ProductDetailsPageLocators.CART_ICON)
 
-	@allure.step("the user waits until the item count in cart is updated to {expected_count}")
-	def wait_until_item_count_is_updated(self, expected_count: int, attempts: int = 5) -> None:
-		for _ in range(attempts):
-			try:
-				element = self.wait_for_element(ProductDetailsPageLocators.COUNT_ITEM_IN_CART_VALUE)
-				actual_count = int(element.text)
-				if actual_count == expected_count:
-					allure.attach(
-						f"Item count in cart updated to {expected_count}.",
-						name="Item Count Update",
-						attachment_type=allure.attachment_type.TEXT)
-				return
-			except:
-				continue
-		raise TimeoutError(f"Item count in cart did not update to {expected_count} within the given time.")
+    @allure.step("the user increases the item count by clicking the '+' button")
+    def increase_item_count(self, items_count: int) -> None:
+        for _ in range(items_count):
+            self.tap_element(ProductDetailsPageLocators.INCREASE_QUANTITY_BUTTON)
 
+    @allure.step("the user waits until the item count in cart is updated to {expected_count}")
+    def wait_until_item_count_is_updated(self, expected_count: int, attempts: int = 5) -> None:
+        for _ in range(attempts):
+            try:
+                element = self.wait_for_element(ProductDetailsPageLocators.COUNT_ITEM_IN_CART_VALUE)
+                actual_count = int(element.text)
+                if actual_count == expected_count:
+                    allure.attach(
+                        f"Item count in cart updated to {expected_count}.",
+                        name="Item Count Update",
+                        attachment_type=allure.attachment_type.TEXT,
+                    )
+                return
+            except:
+                continue
+        raise TimeoutError(f"Item count in cart did not update to {expected_count} within the given time.")
